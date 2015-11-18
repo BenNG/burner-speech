d571da8f7ebc720845c7c842c2d7e778e936ed2f
BenNG <benjamin.kim.nguyen@gmail.com>
2015-11-18 17:03:41 +0100

    feat(burner-speech) init project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var Hello = React.createClass({
+    render: function(){
+        return (
+            <div>Hello World!</div>
+        );
+    }
+});
+
+export default Hello;
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: 'index.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, loader: "babel"},
+            {test: /\.css$/, loader: "style!css"}
+        ]
+    }
+};
