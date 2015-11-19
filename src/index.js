dc3407a2abff227513e63000e6840379fed77362
BenNG <benjamin.kim.nguyen@gmail.com>
2015-11-19 17:52:15 +0100

    add devtools

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 var Hello = React.createClass({
     render: function(){
         return (
-            <div>Hello World!</div>
+            <div>Hello World!!</div>
         );
     }
 });
